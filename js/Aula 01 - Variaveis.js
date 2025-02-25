04b5cc74fe32943174edcc906d33d71e786a7d0f
Filipe520 <filipealves520@gmail.com>
2025-02-25 20:16:06 -0300

    Módulo 01

---
+++ js/Aula 01 - Variaveis.js
@@ -0,0 +1,24 @@
+//  Variável
+let idade = 90  // Type number
+
+let nome = "Filipe" // type String
+
+let logado = true // Type Booleanos
+
+let listaBolo = ['ovo', 'massa', 'corante'] // type Array
+
+let nomeCompleto = {    // Type Objeto
+    nome: 'Filipe',
+    sobrenome: 'Alves',
+    idade: 26
+}
+
+// O comando abaixo exibe os dados no CONSOLE
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof logado)
+console.log(typeof listaBolo)
+console.log(typeof nomeCompleto)
+
+console.log(`Welcome ${nome}! your age is ${idade} idade`) // Templete String
---
+++ js/aula 02 - Comentário no código.js
@@ -0,0 +1,10 @@
+// Este é um comentário
+// oi
+// Nada disso vai aparecer no meu código na execução
+
+/*
+    Vários comentários em várias linhas agente usar esse cara...
+
+    Oi, IUUU, iuuu 
+    Hellow, Word!
+*/
